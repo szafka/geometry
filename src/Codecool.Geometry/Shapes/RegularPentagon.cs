068fdb6af31ceb6145d73da70614ad8edda09fa3
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-13 13:14:06 +0200

    Add RegularPentagon Data and Formulas

--- src/Codecool.Geometry/Shapes/RegularPentagon.cs
+++ src/Codecool.Geometry/Shapes/RegularPentagon.cs
@@ -7,20 +7,29 @@ namespace Codecool.Geometry.Shapes
     /// </summary>
     public class RegularPentagon : Shape
     {
+        private readonly double a;
+        private readonly double n;
+
+        public RegularPentagon(double a, double n)
+        {
+            this.a = a;
+            this.n = n;
+        }
+
         /// <summary>
         ///     Gets formula for the area of the pentagon as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => Console.WriteLine("(n/2)*((a/2)*cosec(PI/n)^2)*sin(2PI/n)");
 
         /// <summary>
         ///     Gets formula for the perimeter of the pentagon as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula => Console.WriteLine("Perimental = 5*a");
 
         /// <inheritdoc/>
-        public override double Area => throw new NotImplementedException();
+        public override double Area => (n/2)*(((a/2)*1/(Math.SIN(Math.PI/n))^2)*Math.SIN(2*Math.PI/n);
 
         /// <inheritdoc/>
-        public override double Perimeter => throw new NotImplementedException();
+        public override double Perimeter => 5*a;
     }
 }
