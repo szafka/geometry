7d457b6085ce3a86967f90cf15c557082d8d3f01
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-13 12:08:13 +0200

    Add dates to EquilateralTriangle and Formulas

--- src/Codecool.Geometry/Shapes/EquilateralTriangle.cs
+++ src/Codecool.Geometry/Shapes/EquilateralTriangle.cs
@@ -7,14 +7,25 @@ namespace Codecool.Geometry.Shapes
     /// </summary>
     public class EquilateralTriangle : Triangle
     {
+        private readonly double a;
+        private readonly double b;
+        private readonly double c;
+        private readonly double h;
+        public EquilateralTriangle(double a, double b, double c, double h)
+        {
+            this.a = a;
+            this.b = a;
+            this.c = a;
+            this.h = h;
+        }
         /// <summary>
         ///     Gets formula for the area of the triangle as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => 0,5 * a * h;
 
         /// <summary>
         ///     Gets formula for the perimeter of the triangle as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula => 3*a;
     }
 }
