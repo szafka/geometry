16e5c7244c53658cd6994bccb4d693989ab8b134
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-13 11:22:32 +0200

    Add Circle dates and Formulas

--- src/Codecool.Geometry/Shapes/Circle.cs
+++ src/Codecool.Geometry/Shapes/Circle.cs
@@ -7,20 +7,27 @@ namespace Codecool.Geometry.Shapes
     /// </summary>
     public class Circle : Shape
     {
+        private readonly double r;
+        public const double PI = 3.1415926535897931;
+        public Circle (double r)
+        {
+            this.PI = PI;
+            this.r = r;
+        }
         /// <summary>
         ///     Gets formula for the area of the circle as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => Console.WriteLine("Area = PI * r^2");
 
         /// <summary>
         ///     Gets formula for the perimeter of the circle as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula => Console.WriteLine("Perimeter = 2*PI*r");
 
         /// <inheritdoc />
-        public override double Area => throw new NotImplementedException();
-
+        public override double Area => PI*r*r;
+        Math.
         /// <inheritdoc />
-        public override double Perimeter => throw new NotImplementedException();
+        public override double Perimeter => 2*PI*r;
     }
-}
+} 
