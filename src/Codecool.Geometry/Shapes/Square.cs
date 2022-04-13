df451721c58f9335b7cebe1c9cd74460e3505014
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-13 17:46:14 +0200

    Add data and Formulas

--- src/Codecool.Geometry/Shapes/Square.cs
+++ src/Codecool.Geometry/Shapes/Square.cs
@@ -6,15 +6,24 @@ namespace Codecool.Geometry.Shapes
     ///     Square implementation class
     /// </summary>
     public class Square : Rectangle
+
+        private readonly double a;
+        private readonly double b;
+
+        public Square(double a, double b)
+         {
+             this.a = a;
+             this.b = a;
+         }
     {
         /// <summary>
         ///     Gets formula for the area of the rectangle as a string.
         /// </summary>
-        public new static string AreaFormula => throw new NotImplementedException();
+        public new static string AreaFormula => Console.WriteLine("Area = a^2");
 
         /// <summary>
         ///     Gets formula for the perimeter of the rectangle as a string.
         /// </summary>
-        public new static string PerimeterFormula => throw new NotImplementedException();
+        public new static string PerimeterFormula => Console.WriteLine("Perimenter = 4*a");
     }
 }
