eb77ff57ed697ac929920753d44a8e72bbfaad56
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-14 14:06:04 +0200

    Create working class, i hope.

---
+++ src/Codecool.Geometry/Shapes/ShapeCollection.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Codecool.Geometry.Shapes
+{
+    public class ShapeCollection : Shape
+    {
+        public override double Area => throw new NotImplementedException();
+        public override double Perimeter => throw new NotImplementedException();
+
+    }
+
+}
