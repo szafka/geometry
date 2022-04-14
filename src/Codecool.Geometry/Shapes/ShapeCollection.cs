f54af3a8962e0eff1fbfe3d8f40482e12ee5df91
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-14 22:40:02 +0200

    Create new List and add addShape metod

--- src/Codecool.Geometry/Shapes/ShapeCollection.cs
+++ src/Codecool.Geometry/Shapes/ShapeCollection.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace Codecool.Geometry.Shapes
 {
-    public class ShapeCollection : Shape
+    public class ShapeCollection
     {
-        public override double Area => throw new NotImplementedException();
-        public override double Perimeter => throw new NotImplementedException();
-
+        private List<Shape> shapeList = new List<Shape>();
+        public void addShape(Shape shape) 
+        {
+        shapeList.Add(shape);
+        }
+        
+        
+        
+        
+        /*public string GeometricName { get; }
+        public double GeometricArea { get; }
+        public double GeoAreaFormula { get; }
+        public double GeometricParameter { get; }
+        public double GeoParaFormula { get; }
+        */
     }
 
 }
